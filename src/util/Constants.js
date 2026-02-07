41acf52e990dcad6181c0684e1a825526a932428
MikeyTheProgrammer <MikeyTheProgrammer@users.noreply.github.com>
2026-02-07 17:11:11 +0000

    Update supported WhatsApp Web version to v2.3000.1033100684

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -7,7 +7,7 @@ exports.DefaultOptions = {
         headless: true,
         defaultViewport: null
     },
-    webVersion: '2.3000.1017054665',
+    webVersion: '2.3000.1033100684',
     webVersionCache: {
         type: 'local',
     },
